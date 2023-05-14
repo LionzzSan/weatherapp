cf2c32fa6f775e9edf31a9312c3c93fc9e8965fc
oleksandrbunin <sasha192.bunin@gmail.com>
2023-05-14 14:42:59 +0300

    save locaiton dto

--- src/main/java/com/lionzzsan/weatherapp/service/ILocationService.java
+++ src/main/java/com/lionzzsan/weatherapp/service/ILocationService.java
@@ -1,12 +1,13 @@
 package com.lionzzsan.weatherapp.service;
 
 import com.lionzzsan.weatherapp.domain.Location;
+import com.lionzzsan.weatherapp.dto.SaveLocationDto;
 
 import java.util.UUID;
 
 public interface ILocationService {
 
-    UUID add();
+    UUID add(SaveLocationDto saveLocationDto);
 
     Iterable<Location> findAll(int page, int pageSize);
 
